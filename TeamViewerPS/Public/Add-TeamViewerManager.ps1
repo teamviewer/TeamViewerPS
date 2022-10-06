b956c09ead31ff422f1fa3600e80e25a6e7363d8
Maximilian Otter <maximilian.otter@outlook.com>
2022-10-06 10:03:54 +0200

    Update Add-TeamViewerManager.ps1
    included adding UserGroups as manager

--- TeamViewerPS/Public/Add-TeamViewerManager.ps1
+++ TeamViewerPS/Public/Add-TeamViewerManager.ps1
@@ -23,9 +23,17 @@ function Add-TeamViewerManager {
         [object]
         $User,
 
+        [Parameter(Mandatory = $true, ParameterSetName = 'Group_ByUserGroupId')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Device_ByUserGroupId')]
+        [ValidateScript( { $_ | Resolve-TeamViewerUserGroupId })]
+        [Alias('UserGroupId')]
+        [object]
+        $UserGroup,
+
         [Parameter(Mandatory = $true, ParameterSetName = 'Group_ByAccountId')]
         [Parameter(Mandatory = $true, ParameterSetName = 'Group_ByManagerId')]
         [Parameter(Mandatory = $true, ParameterSetName = 'Group_ByUserObject')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Group_ByUserGroupId')]
         [ValidateScript( { $_ | Resolve-TeamViewerManagedGroupId } )]
         [Alias("GroupId")]
         [object]
@@ -34,6 +42,7 @@ function Add-TeamViewerManager {
         [Parameter(Mandatory = $true, ParameterSetName = 'Device_ByAccountId')]
         [Parameter(Mandatory = $true, ParameterSetName = 'Device_ByManagerId')]
         [Parameter(Mandatory = $true, ParameterSetName = 'Device_ByUserObject')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Device_ByUserGroupId')]
         [ValidateScript( { $_ | Resolve-TeamViewerManagedDeviceId } )]
         [Alias("DeviceId")]
         [object]
@@ -69,6 +78,9 @@ function Add-TeamViewerManager {
         }
         '*ByUserObject' {
             $body["accountId"] = $User.Id.TrimStart('u')
+        }
+        '*ByUserGroupId' {
+            $body["usergroupId"] = $UserGroup | Resolve-TeamViewerUserGroupId
         }
     }
 
