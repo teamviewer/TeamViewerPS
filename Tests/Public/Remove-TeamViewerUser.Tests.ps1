ce38e6fb0a82ffcb3f89bb62bd48e3a4da218a58
Maximilian Otter <maximilian.otter@agrana.com>
2022-10-24 13:14:00 +0200

    added test for Remove-TeamViewerUser

---
+++ Tests/Public/Remove-TeamViewerUser.Tests.ps1
@@ -0,0 +1,57 @@
+BeforeAll {
+    . "$PSScriptRoot/../../TeamViewerPS/Public/Remove-TeamViewerUser.ps1"
+
+    @(Get-ChildItem -Path "$PSScriptRoot/../../TeamViewerPS/Private/*.ps1") | `
+        ForEach-Object { . $_.FullName }
+
+    $testApiToken = [securestring]@{}
+    $null = $testApiToken
+
+    Mock Get-TeamViewerApiUri { '//unit.test' }
+    Mock Invoke-TeamViewerRestMethod { }
+}
+
+Describe 'Remove-TeamViewerUser' {
+    It 'Should call the correct API endpoint' {
+        Remove-TeamViewerUser -ApiToken $testApiToken -User 'u1234'
+
+        Assert-MockCalled Invoke-TeamViewerRestMethod -Times 1 -Scope It -ParameterFilter {
+            $ApiToken -eq $testApiToken -And `
+                $Uri -eq '//unit.test/users/u1234' -And `
+                $Method -eq 'Delete' }
+    }
+
+    It 'Should accept group objects' {
+        $testUser = @{ id = 'u1234' } | ConvertTo-TeamViewerUser
+        Remove-TeamViewerUser -ApiToken $testApiToken -User $testUser
+
+        Assert-MockCalled Invoke-TeamViewerRestMethod -Times 1 -Scope It -ParameterFilter {
+            $ApiToken -eq $testApiToken -And `
+                $Uri -eq '//unit.test/users/u1234' -And `
+                $Method -eq 'Delete' }
+    }
+
+    It 'Should fail for invalid group identifiers' {
+        { Remove-TeamViewerUser -ApiToken $testApiToken -User 'invalid1234' } | Should -Throw
+    }
+
+    It 'Should accept pipeline input' {
+        $testUser = @{ id = 'u1234' } | ConvertTo-TeamViewerUser
+        $testUser | Remove-TeamViewerUser -ApiToken $testApiToken
+
+        Assert-MockCalled Invoke-TeamViewerRestMethod -Times 1 -Scope It -ParameterFilter {
+            $ApiToken -eq $testApiToken -And `
+                $Uri -eq '//unit.test/users/u1234' -And `
+                $Method -eq 'Delete' }
+    }
+
+    It 'Should accept switch parameter "Permanent"' {
+        $testUser = @{ id = 'u1234' } | ConvertTo-TeamViewerUser
+        Remove-TeamViewerUser -ApiToken $testApiToken -User $testUser -Permanent
+
+        Assert-MockCalled Invoke-TeamViewerRestMethod -Times 1 -Scope It -ParameterFilter {
+            $ApiToken -eq $testApiToken -And `
+                $Uri -eq '//unit.test/users/u1234?isPermanentDelete=true' -And `
+                $Method -eq 'Delete' }
+    }
+}
