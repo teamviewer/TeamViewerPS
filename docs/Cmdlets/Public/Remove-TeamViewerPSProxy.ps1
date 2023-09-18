1530dc705d868f29a9f6d5a0692b0b8ef3d5834d
karthickgandhiTV <karthick.gandhi@teamviewer.com>
2023-09-18 16:31:40 +0200

    Remove-TeamViewerPSProxy ShouldProcess block added

--- docs/Cmdlets/Public/Remove-TeamViewerPSProxy.ps1
+++ docs/Cmdlets/Public/Remove-TeamViewerPSProxy.ps1
@@ -1,11 +1,13 @@
 function Remove-TeamViewerPSProxy {
     [CmdletBinding(SupportsShouldProcess = $true)]
     param()
-    
-    $global:TeamViewerProxyUriSet = $null
-    $global:TeamViewerProxyUriSet | Out-Null  # https://github.com/PowerShell/PSScriptAnalyzer/issues/1472
+
     $global:TeamViewerProxyUriRemoved = $true
     $global:TeamViewerProxyUriRemoved | Out-Null  # https://github.com/PowerShell/PSScriptAnalyzer/issues/1472
-
+    
+    if($PSCmdlet.ShouldProcess($TeamViewerProxyUriRemoved,"Remove proxy for WebAPI")){
+    $global:TeamViewerProxyUriSet = $null
+    $global:TeamViewerProxyUriSet | Out-Null  # https://github.com/PowerShell/PSScriptAnalyzer/issues/1472
     [Environment]::SetEnvironmentVariable('TeamViewerProxyUri','', 'User')
+    }
 }
