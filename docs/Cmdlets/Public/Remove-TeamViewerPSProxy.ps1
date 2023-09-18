0bdd16a8c45b58e5a9f4fb074ffb890b196c07da
karthickgandhiTV <karthick.gandhi@teamviewer.com>
2023-09-18 16:25:09 +0200

    PSUseShouldProcessForStateChangingFunction Warning fixed

--- docs/Cmdlets/Public/Remove-TeamViewerPSProxy.ps1
+++ docs/Cmdlets/Public/Remove-TeamViewerPSProxy.ps1
@@ -1,7 +1,10 @@
 function Remove-TeamViewerPSProxy {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param()
+    
     $global:TeamViewerProxyUriSet = $null
     $global:TeamViewerProxyUriSet | Out-Null  # https://github.com/PowerShell/PSScriptAnalyzer/issues/1472
-    $global:TeamViewerProxyUriRemoved = $true  
+    $global:TeamViewerProxyUriRemoved = $true
     $global:TeamViewerProxyUriRemoved | Out-Null  # https://github.com/PowerShell/PSScriptAnalyzer/issues/1472
 
     [Environment]::SetEnvironmentVariable('TeamViewerProxyUri','', 'User')
--- docs/Cmdlets/Public/Set-TeamViewerPSProxy.ps1
+++ docs/Cmdlets/Public/Set-TeamViewerPSProxy.ps1
@@ -1,18 +1,21 @@
 $global:TeamViewerProxyUriSet = $null
 
 function Set-TeamViewerPSProxy {
+    [CmdletBinding(SupportsShouldProcess =$true)]
     param (
         [Parameter(Mandatory=$true)]
         [Uri]
         $ProxyUri
     )
 
+    if($PSCmdlet.ShouldProcess($ProxyUri,"Sets proxy for WebAPI")){
     $global:TeamViewerProxyUriSet = $ProxyUri
     $global:TeamViewerProxyUriRemoved = $false
-    $global:TeamViewerProxyUriRemoved | Out-Null 
+    $global:TeamViewerProxyUriRemoved | Out-Null
 
     [Environment]::SetEnvironmentVariable("TeamViewerProxyUri", $ProxyUri, "User")
 
     Write-Output "Proxy set to $TeamViewerProxyUriSet"
+    }
 }
 
