d870a714d2d4333372a77af26863db2113c493f0
Stefan Hubertus <stefan.hubertus@teamviewer.com>
2023-09-21 10:56:10 +0200

    Revert "Revert "Merge pull request #38 from karthickgandhiTV/main""
    This reverts commit 4c371c8c2016d2a7748e49756231ebd5d5e84f9e.

--- Tests/Public/Add-TeamViewerRoleToUserGroup.Tests.ps1
+++ Tests/Public/Add-TeamViewerRoleToUserGroup.Tests.ps1
@@ -9,7 +9,6 @@ BeforeAll {
     $null = $testUserGroup
     $testUserRoleId = '9b465ea2-2f75-4101-a057-58a81ed0e57b'
     $null = $testUserRoleId
-
     Mock Get-TeamViewerApiUri { '//unit.test' }
     $mockArgs = @{}
     Mock Invoke-TeamViewerRestMethod { $mockArgs.Body = $Body
@@ -20,10 +19,8 @@ BeforeAll {
     }
 }
 Describe 'Add-TeamViewerRoleToUserGroup' {
-
     It 'Should call the correct API endpoint' {
         Add-TeamViewerRoleToUserGroup -ApiToken $testApiToken -UserRoleId $testUserRoleId -UserGroup $testUserGroup
-
         Assert-MockCalled Invoke-TeamViewerRestMethod -Times 1 -Scope It -ParameterFilter {
             $ApiToken -eq $testApiToken -And `
                 $Uri -eq '//unit.test/userroles/assign/usergroup' -And `
