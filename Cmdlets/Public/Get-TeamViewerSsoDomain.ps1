750932f00677953a66c56cdc89198dd5761bd2e8
radu-cristea-15 <radu.cristea@teamviewer,com>
2025-10-22 14:02:23 +0200

    Update Get-TeamViewerSsoDomain.Tests.ps1 Add new endpoint ID for function Get-TeamViewerSsoDomain.ps1

--- Cmdlets/Public/Get-TeamViewerSsoDomain.ps1
+++ Cmdlets/Public/Get-TeamViewerSsoDomain.ps1
@@ -1,15 +1,30 @@
 function Get-TeamViewerSsoDomain {
+    [CmdletBinding(DefaultParameterSetName = "FilteredList")]
     param(
         [Parameter(Mandatory = $true)]
         [securestring]
-        $ApiToken
+        $ApiToken,
+        
+        [Parameter(ParameterSetName = "ByDomainId")]
+        [Alias("DomainId")]
+        [guid]
+        $Id
     )
 
     $resourceUri = "$(Get-TeamViewerApiUri)/ssoDomain";
+    $parameters = @{ }
+    switch ($PsCmdlet.ParameterSetName) {
+        'ByDomainId' {
+            $resourceUri += "/$Id"
+            $parameters = $null
+        }
+    }
+
     $response = Invoke-TeamViewerRestMethod `
         -ApiToken $ApiToken `
         -Uri $resourceUri `
         -Method Get `
+        -Body $parameters `
         -WriteErrorTo $PSCmdlet `
         -ErrorAction Stop
     Write-Output ($response.domains | ConvertTo-TeamViewerSsoDomain)
