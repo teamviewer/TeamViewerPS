2ba8bb2fe624b53dd30f211ca3e46c78588908b4
christian.jaeckle <christian.jaeckle@teamviewer.com>
2026-08-09 23:38:26 +0200

    Fix Get-TeamViewerInstallationPackage.ps1

--- Cmdlets/Public/Get-TeamViewerInstallationPackage.ps1
+++ Cmdlets/Public/Get-TeamViewerInstallationPackage.ps1
@@ -1,5 +1,7 @@
 function Get-TeamViewerInstallationPackage {
     if (Test-TeamViewerInstallation) {
+        $TV_InstallationDirectory = Get-TeamViewerInstallationDirectory
+
         $Package = (Get-Item -Path (Join-Path -Path $TV_InstallationDirectory -ChildPath 'TeamViewer.exe')).VersionInfo.ProductName
 
         switch -Wildcard ($Package) {
